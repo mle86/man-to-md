a908902c64790816ae68f27afe66f6f600e81efc
mle <maximilian@eul.cc>
2017-07-02 15:44:23 +0200

    test: extracted 01-breaks from 01-specialchars

---
+++ test/01-test-breaks.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+. $(dirname "$0")/init.sh
+
+
+# There's a single linebreak between line1 and line2, which should collapse to a space.
+# There's a .br between line2 and line3, which should get converted to a paragraph.
+# There's a .P  between line3 and line4, which should get converted to a paragraph.
+output="$(conv_sample1 | get_section 'BREAKS')"
+
+test_pcre "$output" 'line 1( |\n)line 2' \
+	"Contiguous lines did not get converted correctly!"
+test_pcre "$output" 'line 2\n\nline 3' \
+	"Lines separated by .br did not get converted correctly!"
+test_pcre "$output" 'line 3\n\nline 4' \
+	"Lines separated by .P did not get converted correctly!"
+
+
+success
