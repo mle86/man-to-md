e74c34dd470cbdbd75a0fd2413c6aecec2be3506
mle <maximilian@eul.cc>
2017-07-02 16:06:50 +0200

    test sections and subsections (.SH, .SS)

---
+++ test/01-test-sections.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+. $(dirname "$0")/init.sh
+
+output="$(conv_sample1)"
+
+
+match_section_title () {
+	local input="$1"
+	local sectionlevel="$2"
+	local sectiontitle="$3"
+	local regex=
+
+	# We don't test for Titlecase here, there's a specialized test just for that:
+	regex="$regex""(?i)"
+	# Section and Subsection titles should be preceded by at least one blank line:
+	regex="$regex""\n\n+"
+	# Start with no more than three spaces (lest they be considered a code block):
+	regex="$regex"" {0,3}"
+	# One hash# per section level, without spaces:
+	regex="$regex""#{${sectionlevel}}"
+	# Have at least one space between the hashes and the section title,
+	regex="$regex"" +${sectiontitle}"
+	# End with no more than one space:
+	regex="$regex"" ?"
+	# Should be followed by at least one blank line:
+	regex="$regex""\n\n+"
+	# In our test input, there are no empty sections
+	# and all tested section start with some text:
+	regex="$regex""\w"
+
+	test_pcre "$input" "$regex"
+}
+
+
+match_section_title "$output" 1 "Description"		# .SH section
+match_section_title "$output" 1 "Options"		# .SH section
+match_section_title "$output" 2 "Texty Subsection"	# .SS subsection
+
+
+success
