mle86/man-to-md

commit dae866b350a37f4586a99c248cdfe2ebf68f081b

html: convert ">" to "&gt;"
closes #2

man-to-md.pl
@@ -231,7 +231,11 @@ sub strip_highlighting {
 
 sub strip_html {
 	# avoid accidental html output:
-	my @result = map{ s/</&lt;/g; $_ } ($#_ >= 0 ? @_ : ($_));
+	my @result = map{
+			s/</&lt;/g;
+			s/>/&gt;/g;
+		$_ }
+		($#_ >= 0 ? @_ : ($_));
 	wantarray ? @result : $result[0]
 }
 

test/01-test-html.sh
@@ -3,7 +3,7 @@
 
 
 LT='&lt;'  # plain "<" is NOT acceptable!
-GT='(?:>|&gt;)'
+GT='&gt;'
 # fake program name is "a<b>"
 PROG="a${LT}b${GT}"
 PLAINPROG="a<b>"

commit ea1e8f5a6ad69b3cebc0226be3a2566367afee64

fix author url bug
Fixes a bug introduced in commit dae866b.

In the AUTHOR section,
e-mails automatically get converted to mailto: links.
Also the "author name <author@address>" line
is sometimes followed by an "(https://author.tld)" line
which is our magic format to turn the previous line into a labeled link.

There was an exception in the output postprocessing fn
to not do that when the previous line ends with a closing angle bracket (>)
because it indicates an e-mail link
but since commit dae866b we replace closing angle brackts with the "&gt;" entity.

This commit extends the exception so it will recognize "&gt;" as well.

man-to-md.pl
@@ -127,7 +127,7 @@ sub {
 
 	# URLs:
 	my $re_urlprefix = '(?:https?:|s?ftp:|www)';
-	s/^(.+[^)>])(?:$)\n^(?:[\[\(]\*{0,2}(${re_urlprefix}.+?)\*{0,2}[\]\)])([\s,;\.\?!]*)$/[$1]($2)$3/gm;
+	s/^(.+)(?<!&gt;)(?<!>)(?:$)\n^(?:[\[\(]\*{0,2}(${re_urlprefix}.+?)\*{0,2}[\]\)])([\s,;\.\?!]*)$/[$1]($2)$3/gm;
 
 	# Line breaks;
 	s/\n *${replacement_token}#BRK#/  \n/g;

test/90-test-bug-authormailurl.sh
@@ -1,0 +1,61 @@
+#!/bin/sh
+. $(dirname "$0")/init.sh
+
+output="$(conv author.roff | get_section 'AUTHOR')"
+
+authorName='AuthorName'
+authorMail='author@address.tld'
+authorAddr='https://author.homepage/'
+
+otherName='OtherAuthor'
+otherMail=''
+otherAddr='https://other-author.homepage/'
+
+thirdName='ThirdAuthor'
+thirdMail='third@author.tld'
+thirdAddr=''
+
+
+
+assertRegex "$output" '/\sAuthorName\s+(?:<|&lt;)\[author@address.tld\]\(mailto:author@address.tld\)(?:>|&gt;)\s+\(https:\/\/author.homepage\/\)\s/'
+
+	# Author name with e-mail address AND url in next line.
+	# The name-and-mail line should NOT be turned into a link.
+	## A0
+	## AuthorName <author@address.tld>
+	## (https://author.homepage/)
+	## A1
+
+	# Expected:
+	## A0
+	## AuthorName &lt;[author@address.tld](mailto:author@address.tld&gt)&gt;
+	## (https://author.homepage/)
+	## A1
+
+	# NOT expected:
+	## A0
+	## [AuthorName &lt;[author@address.tld](mailto:author@address.tld&gt)&gt;](https://author.homepage/)
+	## A1
+
+assertRegex "$output" '/\s\[OtherAuthor\]\(https:\/\/other-author.homepage\/\)\s/'
+
+	## O0
+	## OtherAuthor
+	## (https://other-author.homepage/)
+	## O1
+
+assertRegex "$output" '/\sThirdAuthor\s+(?:<|&lt;)\[third@author.tld\]\(mailto:third@author.tld\)(?:>|&gt;)\s/'
+
+	# Text block with a simple name followed by an e-mail addres
+	# (which should be turned into a link).
+	## T0
+	## ThirdAuthor <third@author.tld>
+	## T1
+
+	# Expected Output:
+	## T0
+	## ThirdAuthor &lt;[third@author.tld](mailto:third@author.tld)&gt;
+	## T1
+
+
+success

test/samples/author.roff
@@ -1,0 +1,16 @@
+.TH TESTPROG "7" "February 2019" "testprog 0.9.3" ""
+.SH AUTHOR
+A0
+AuthorName <author@address.tld>
+(https://author.homepage/)
+A1
+.P
+O0
+OtherAuthor
+(https://other-author.homepage/)
+O1
+.P
+T0
+ThirdAuthor <third@author.tld>
+T1
+.P