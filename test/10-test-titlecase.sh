fd771c147181eee27aed205c3c00bcabdddd3086
mle <maximilian@eul.cc>
2019-11-27 23:42:10 +0100

    --word: recognize underscores in words
    This change allows us to add words with underscores to the word list.
    
    For example, the section title “.SH THE ENV_VAR VARIABLE”
    can now be turned into “# The ENV_VAR Variable”
    if we supply the “--word ENV_VAR” option.

--- test/10-test-titlecase.sh
+++ test/10-test-titlecase.sh
@@ -2,11 +2,16 @@
 . $(dirname "$0")/init.sh
 
 
-words="-w XML --word cRaZyCaPs"
+words="-w XML --word cRaZyCaPs --word ENV_VAR"
 
 assertContains \
 	"$(conv_sample1_headlines $words)" \
 	"A Simple Approach to Title-Casing of Known Words Like XML and cRaZyCaPs" \
+	"The program did not correctly title-case the section headlines!"
+
+assertContains \
+	"$(conv_sample1_headlines $words)" \
+	"The ENV_VAR Variable" \
 	"The program did not correctly title-case the section headlines!"
 
 
