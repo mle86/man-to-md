mle86/man-to-md

commit e13a054ad4cc64493ffb0e6df6bb72062307d709

support .ds string definitions
.ds Y REPLACEMENT
 defines a one-letter string, inserted anywhere with \*Y.
.ds YY REPLACEMENT
 defines a two-letter string, inserted anywhere with \*(YY.

man-to-md.pl
@@ -52,6 +52,7 @@ my %paste_before_section = ( );
 my $code_formatting = 0;
 my $add_comment;
 
+my %strings = ( );
 my %words = ( );
 my %stopwords = map { $_ => 1 } (qw(
 	a an the
@@ -285,7 +286,9 @@ sub strip_highlighting {
 	# other unprintables and control characters:
 	s/\\[\/,]//g;
 
-
+	# unknown \*X or \*(XX string usages not previously defined with .ds:
+	s/\\\*[^\s\(]//g;
+	s/\\\*\([^\s]{2}//g;
 }
 
 sub strip_html {
@@ -340,6 +343,12 @@ sub reformat_syntax {
 	if (m/^\.(?:PD|hy|ad|\s|$)/) {
 		$_ = '';
 		return
+	}
+
+	# replace .ds strings:
+	for my $sname (keys %strings) {
+		if    (length $sname == 1) { s/\\\*$sname/$strings{$sname}/g; }
+		elsif (length $sname == 2) { s/\\\*\($sname/$strings{$sname}/g; }
 	}
 
 	# raw block markers:
@@ -639,6 +648,9 @@ do {
 	} elsif (subsection_title) {
 		# new subsection begins
 		print_subsection_title $subsection
+
+	} elsif (m/^\.ds +(\S{1,2}) +"?(.+)$/) {
+		$strings{ $1 } = $2
 
 	} elsif (m/^\.de\b/) {
 		# macro definition -- skip completely

test/10-test-strings.sh
@@ -1,0 +1,37 @@
+#!/bin/sh
+. $(dirname "$0")/init.sh
+
+
+output="$(conv strings.roff | get_section 'OUTPUT')"
+
+# regex components:
+B='\*\*'
+I='_'
+# .ds Y (SINGLE-Y)
+# .ds  YY  \fB(DOUBLE-Y)\fR
+# .ds  YM  (\fIMULTI-\
+# LINED\fR)
+Y_USAGE="\\(SINGLE-Y\\)"
+YY_USAGE="${B}\\(DOUBLE-Y\\)${B}"
+YM_USAGE="\\(${I}MULTI-LINED${I}\\)"
+
+
+# aaaa\*Ybbbb\*(YYcccc
+# Used _before_ any .ds commands, so the strings should be empty.
+assertRegex "$output" "/\\baaaabbbbcccc\\b/" \
+	"Unknown strings (not yet defined with .ds) did not get replaced with empty string!"
+
+# dddd\*Yeeee\*(YYffff
+# Used correctly after .ds string definitions.
+assertRegex "$output" "/\\bdddd${Y_USAGE}eeee\\b/" \
+	"Single-letter string \\*Y was not replaced correctly!"
+assertRegex "$output" "/\\beeee${YY_USAGE}ffff\\b/" \
+	"Two-letter string \\*(YY was not replaced correctly!"
+
+# gggg\*(YMhhhh
+# Used correctly after .ds string definition, although that .ds definition contains a line continuation.
+assertRegex "$output" "/\\bgggg${YM_USAGE}hhhh\\b/" \
+	"Two-letter string \\*(YM was replaced, but its multi-line .ds defintion was not read correctly!"
+
+
+success

test/samples/strings.roff
@@ -1,0 +1,12 @@
+.TH TESTPROG "7" "September 2019" "testprog 0.9.2" ""
+.SH OUTPUT
+aaaa\*Ybbbb\*(YYcccc
+.P
+.ds Y (SINGLE-Y)
+.ds  YY  \fB(DOUBLE-Y)\fR
+.ds  YM  (\fIMULTI-\
+LINED\fR)
+dddd\*Yeeee\*(YYffff
+.P
+gggg\*(YMhhhh
+.P

commit b63e0ae088d1b5d1506f7e39cb8af7619e4fd0a6

fix escape problem (backslash + asterisk combination)
This fixes a rare problem introduced in e13a054ad (v0.14.0).

man-to-md.pl
@@ -303,8 +303,13 @@ sub strip_highlighting {
 	s/\\[\/,]//g;
 
 	# unknown \*X or \*(XX string usages not previously defined with .ds:
-	s/\\\*[^\s\(]//g;
-	s/\\\*\([^\s]{2}//g;
+	s/^(?:\\[^\*]|[^\\])*?\K\\\*[^\s\(]//g;
+	s/^(?:\\[^\*]|[^\\])*?\K\\\*\([^\s]{2}//g;
+	# These regexes look a bit weird.
+	# They prevent removal of non-string-sequence input like ...**\\\\**...
+	# but excluding there patterns with a negative look-behind
+	# won't work because it's not a fixed-length match.
+	# TODO: Apply similar exclusions to all other backslash-escaped replacements in this sub?
 
 	utf8::encode($_);
 }

test/90-test-bug-escaping.sh
@@ -1,0 +1,19 @@
+#!/bin/sh
+. $(dirname "$0")/init.sh
+
+
+backslash="\\"
+
+# input:
+	# Backslashes in the input will be replaced with a literal \fB\\\\\fR,
+	# while linebreaks in the input will be replaced with a literal \fB\\n\fR.
+define expectedOutput <<-EOT
+	Backslashes in the input will be replaced with a literal **${backslash}${backslash}${backslash}${backslash}**,
+	while linebreaks in the input will be replaced with a literal **${backslash}${backslash}n**.
+EOT
+
+
+output="$(conv escaping.roff | get_section 'BACKSLASHES AND ASTERISKS')"
+assertEq "$output" "$expectedOutput"
+
+success

test/samples/escaping.roff
@@ -1,0 +1,4 @@
+.TH CSV "1" "December 2019" "csv-parser 2.6.0" ""
+.SH "BACKSLASHES AND ASTERISKS"
+Backslashes in the input will be replaced with a literal \fB\\\\\fR,
+while linebreaks in the input will be replaced with a literal \fB\\n\fR.